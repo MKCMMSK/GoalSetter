c5e2410b44a3934095e12d6416c5bfee67e7807e
Colin <colinmaisk@gmail.com>
2020-06-07 16:31:39 -0700

    created .env and secured database details to it

--- backend/database/schema.py
+++ backend/database/schema.py
@@ -1,10 +1,14 @@
+import os
+import urllib.parse as up
 import psycopg2
-
-conn = psycopg2.connect(database="wiqbhzyf",
-user="wiqbhzyf",
-password="46dqd7zYzR6p8XBQgoZsDlRzG8DFQHf0",
-host="ruby.db.elephantsql.com",
-port="5432"
+from dotenv import load_dotenv
+load_dotenv()
+url = up.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
 )
 
 print("successful connection")
