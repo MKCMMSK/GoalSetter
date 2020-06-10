ffd9bea41a506dda9bb86d34e59f0e5d66b150f6
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-06-09 20:41:04 -0400

    add swipable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Swipable from "./swipable"
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <Swipable/>
       </header>
     </div>
   );
