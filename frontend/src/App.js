f08bf365fdd4b72b77a8a86822b566b234135c4e
Liz <ej2brown@uwaterloo.ca>
2020-06-11 16:02:02 -0400

    move data to maincontainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,33 +2,27 @@ import React from 'react';
 import './App.css';
 
 //components
-import NavBar from "./components/Navigation"
+import NavBar from "./components/NavBar"
 import Swipable from "./components/Swipable"
 import Tasks from "./components/Tasks"
 import MonthlyCalendar from "./components/Calendar"
-import Dragable from "./components/Dragable"
+import SessionList from "./components/SessionList"
+
+const sessions = [
+  { id: 1, duration: '00:00:30', time_of_day: '2020-06-06', productivity: 'TRUE', site_id: 1 },
+  { id: 2, duration: '01:00:30', time_of_day: '2020-06-06', productivity: 'FALSE', site_id: 2 },
+  { id: 3, duration: '02:00:30', time_of_day: '2020-06-06', productivity: 'TRUE', site_id: 1 },
+  { id: 4, duration: '00:10:30', time_of_day: '2020-06-07', productivity: 'TRUE', site_id: 4 },
+  { id: 5, duration: '00:30:30', time_of_day: '2020-06-07', productivity: 'FALSE', site_id: 1 }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <nav className="App-header">
         <NavBar />
-      </header>
-      <main className="App-body">
-
-        <h1>Dashboad Page</h1>
-        <section className="dashboad-page">
-          <MonthlyCalendar />
-          <Swipable  />
-        </section>
-
-        <h1>Project Page</h1>
-        <section className="project-page">
-          <Tasks />
-          {/* <Dragable/> */}
-        </section>
-
-      </main>
+      </nav>
+      <MainContainer />
     </div>
   );
 }
