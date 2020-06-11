9e25c9cb5bbabf827bd63098e97cdf57bcb8e04a
Liz <ej2brown@uwaterloo.ca>
2020-06-11 16:01:00 -0400

    addsessionlist

---
+++ frontend/src/components/SessionList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import SessionListItem from "./SessionListItem.js";
+
+export default function SessionList(props) {
+    const { sessions } = props;
+    
+    const SessionList = sessions.map((session) => {
+        console.log(session.time_of_day)
+        return (
+            <SessionListItem
+                key={session.id}
+                duration={session.duration}
+                time_of_day={session.time_of_day}
+                productivity={session.productivity}
+                site_id={session.site_id}
+            />
+        );
+    });
+
+    return <ul>{SessionList}</ul>;
+}
