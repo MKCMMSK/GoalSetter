382835e18096f5a0711fecfa7f4c3f8da1b4f64c
Colin <colinmaisk@gmail.com>
2020-06-12 13:03:37 -0700

    removed users comments

--- users/models.py
+++ users/models.py
@@ -6,12 +6,3 @@ class User(models.Model):
     last_name = models.CharField(max_length=20)
     email = models.EmailField(max_length=100, unique=True)
     
-
-
-
-
-
-
-    # first_name VARCHAR(25) NOT NULL,
-    # last_name VARCHAR(25) NOT NULL,
-    # email VARCHAR(50) UNIQUE NOT NULL
