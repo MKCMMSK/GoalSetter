63a3c48ff38d06f45cdac6fe3f19ec44d74eafe3
Colin <colinmaisk@gmail.com>
2020-06-12 12:51:47 -0700

    created app for projects,sessions, sites, tasks, and whitelists and setup user migrations

--- users/models.py
+++ users/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100, unique=True)
     
+
+
+
+
+
+
+    # first_name VARCHAR(25) NOT NULL,
+    # last_name VARCHAR(25) NOT NULL,
+    # email VARCHAR(50) UNIQUE NOT NULL
