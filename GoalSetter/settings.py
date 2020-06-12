5e558b4d9a3422d1438a8dba9ff9f46bf063feac
Colin <colinmaisk@gmail.com>
2020-06-12 15:17:37 -0700

    created project models not sure if foreign key is correct

--- GoalSetter/settings.py
+++ GoalSetter/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users'
+    'users',
+    'projects',
+    'url_sessions',
+    'sites',
+    'tasks',
+    'whitelists'
 ]
 
 MIDDLEWARE = [
--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Project(models.Model):
-    
-
-#     name VARCHAR(25) UNIQUE NOT NULL,
-#     description TEXT,
-#     start_date TIMESTAMP,
-#     estimate_time INTERVAL,
-#     actual_time INTERVAL,
-#     user_id INT REFERENCES users(id)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    start_date = models.DateField(auto_now=True)
+    estimate_time = models.DurationField
+    actual_time = models.DurationField
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
