e6522be2f7f4c2bb2807708d1bfe1d5cfc20de16
Colin <colinmaisk@gmail.com>
2020-06-12 13:04:36 -0700

    started projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    
+
+#     name VARCHAR(25) UNIQUE NOT NULL,
+#     description TEXT,
+#     start_date TIMESTAMP,
+#     estimate_time INTERVAL,
+#     actual_time INTERVAL,
+#     user_id INT REFERENCES users(id)
