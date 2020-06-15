47dc44d3c0be2011c137c118333e4c552ebb062d
Liz <ej2brown@uwaterloo.ca>
2020-06-15 16:28:42 -0400

    add light/dark theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+
 //components
 import NavBar from "./components/NavBar"
+import LightTheme from './themes/light';
+import DarkTheme from './themes/dark';
+
+const GlobalStyle = createGlobalStyle`
+	body{
+		background: ${p => p.theme.bodyBackgroundColor};
+		min-height: 100vh;
+		margin: 0;
+		color: ${p => p.theme.bodyFontColor};
+		font-family: 'Kaushan Script'
+	}
+`;
 
 function App() {
+  const [theme, setTheme] = useState(LightTheme);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <Routes />
-      </BrowserRouter>
+      <ThemeProvider theme={{
+        ...theme, setTheme: () => {
+          setTheme(s => s.id === 'light' ? DarkTheme : LightTheme);
+        }
+      }}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <NavBar />
+          <Routes />
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
