40a1afc9542ef0b23c679643c153f9bff0f7ea5f
Robert Beckett <robert.beckett@onion.red>
2020-06-16 19:35:45 -0700

    Added event listener for currently active tab/window.

--- extension/popup.js
+++ extension/popup.js
@@ -1,2 +1,21 @@
 const toggleSwitch = document.getElementById('toggle');
 
+let activeTabId = null;
+let activeWindowId = null;
+
+
+
+toggleSwitch.addEventListener((e) => {
+
+});
+
+const startMonitoring = () => {
+    chrome.tabs.onActivated.addListener((tab) => {
+        activeTabID = tab.tabId;
+        activeWindowId = tab.windowId;
+    })
+};
+
+const endMonitoring = () => {
+
+};
