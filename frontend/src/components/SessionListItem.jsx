04695dabf5907892aee549528c93ea946b078e57
Liz <ej2brown@uwaterloo.ca>
2020-06-19 12:46:58 -0400

    remove sessions list

--- frontend/src/components/SessionListItem.jsx
+++ frontend/src/components/SessionListItem.jsx
@@ -1,37 +1,15 @@
-import * as React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import {
-    Scheduler,
-    DayView,
-    Appointments,
-} from '@devexpress/dx-react-scheduler-material-ui';
-
-const currentDate = '2018-11-01';
-const schedulerData = [
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-];
+import React from "react";
 
 export default function SessionListItem(props) {
     const { id, duration, time_of_day, productivity, site_id } = props;
     console.log(time_of_day)
 
-
+    
     return (
-        <Paper>
-            <Scheduler
-                data={schedulerData}
-            >
-                <ViewState
-                    currentDate={currentDate}
-                />
-                <DayView
-                    startDayHour={9}
-                    endDayHour={14}
-                />
-                <Appointments />
-            </Scheduler>
-        </Paper>
+        <ul className={id}>
+            <li >duration: {duration}</li>
+            <li >Time: {time_of_day}</li>
+            <li >site_id: {site_id}</li>
+        </ul>
     );
 }
--- frontend/src/containers/Project.jsx
+++ frontend/src/containers/Project.jsx
@@ -6,14 +6,15 @@ import PageLayout from '../components/PageLayout';
 // import Swipable from "../components/Swipable"
 // import Tasks from "../components/Tasks"
 // import MonthlyCalendar from "../components/Calendar"
-import SessionList from "../components/SessionList"
+// import SessionList from "../components/SessionList"
+import WeekTaskView from "../components/WeekTaskView"
 
 const sessions = [
-  { id: 1, duration: '00:00:30', time_of_day: '2020-06-06', productivity: 'TRUE', site_id: 1 },
-  { id: 2, duration: '01:00:30', time_of_day: '2020-06-06', productivity: 'FALSE', site_id: 2 },
-  { id: 3, duration: '02:00:30', time_of_day: '2020-06-06', productivity: 'TRUE', site_id: 1 },
-  { id: 4, duration: '00:10:30', time_of_day: '2020-06-07', productivity: 'TRUE', site_id: 4 },
-  { id: 5, duration: '00:30:30', time_of_day: '2020-06-07', productivity: 'FALSE', site_id: 1 }
+  { id: 1, duration: '00:00:30', time_of_day: '2020-06-06 01:23:45-04', productivity: 'TRUE', site_id: 1 },
+  { id: 2, duration: '01:00:30', time_of_day: '2020-06-06 02:23:45-04', productivity: 'FALSE', site_id: 2 },
+  { id: 3, duration: '02:00:30', time_of_day: '2020-06-06 03:23:45-04', productivity: 'TRUE', site_id: 1 },
+  { id: 4, duration: '00:10:30', time_of_day: '2020-06-07 04:23:45-04', productivity: 'TRUE', site_id: 4 },
+  { id: 5, duration: '00:30:30', time_of_day: '2020-06-07 05:23:45-04', productivity: 'FALSE', site_id: 1 }
 ]
 
 const ProjectPage = () => {
@@ -38,7 +39,8 @@ const ProjectPage = () => {
       
       <section className="dashboad-page">
         {/* <MonthlyCalendar /> */}
-        <SessionList sessions={sessions} />
+        {/* <SessionList sessions={sessions} /> */}
+        <WeekTaskView/>
         {/* <Swipable  /> */}
       </section>
     </PageLayout>
