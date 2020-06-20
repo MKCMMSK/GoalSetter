5ccc46de6335eea0968773f2603f1ce8d0eae26a
Liz <ej2brown@uwaterloo.ca>
2020-06-20 14:50:07 -0400

    uncomment id=1 hardcoded queries

--- tasks/api.py
+++ tasks/api.py
@@ -5,8 +5,10 @@ from projects.models import Project
 from rest_framework import request
 
 # Task Viewset
+
+
 class TaskViewSet(viewsets.ModelViewSet):
-    
+
     # project = Project.objects.get(id=1)
     # tasks = project.task_set.all()
     # queryset = tasks
@@ -16,4 +18,11 @@ class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
 
     def get_queryset(self):
-        return self.request.query_params.tasks.all()
+        # return self.request.query_params.tasks.all()
+        # deconstruct = self.request.query_params.__getitem__(currentUserId)
+        # deconstruct = self.request.query_params.currentUserId
+        deconstruct = self.request.body.decode('ascii')
+        print(deconstruct, 'THIS IS THE TESTED PRINT')
+        return deconstruct
+        # currentProject = Project.objects.get(id=self.request.query_params)
+        # return currentProject.tasks.all()
