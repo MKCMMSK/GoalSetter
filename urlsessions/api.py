d6b01158e7b1b5e273120db8713c07ac97928df9
Shao Kang Mai <Colinmaisk@gmail.com>
2020-06-21 02:25:08 -0700

    added function to get_sessions_by_week, need testing

--- urlsessions/api.py
+++ urlsessions/api.py
@@ -1,6 +1,11 @@
 from urlsessions.models import URLSession
 from rest_framework import viewsets, permissions
 from .serializers import URLSessionSerializer
+from tasks.models import Task
+from rest_framework import request
+from rest_framework.decorators import action
+from rest_framework.response import Response
+import json
 
 class URLSessionViewSet(viewsets.ModelViewSet):
     queryset = URLSession.objects.all()
@@ -8,3 +13,13 @@ class URLSessionViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = URLSessionSerializer
+
+    @action(detail=True)
+    def get_sessions_by_week(self, request, pk=None):
+        deconstruct = json.loads(self.request.body.decode('ascii'))
+        print(deconstruct)
+        beginningOfWeek = deconstruct["sunday"]
+        endOfWeek = deconstruct["saturday"] 
+        
+        sessionsByWeek = Session.objects.filter(time_of_day__range[beginningOfWeek, endOfWeek])
+        return deconstruct
