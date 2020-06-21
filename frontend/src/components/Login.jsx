69a7ef068da2ed4ddfaf007f353da55bd70c2fcd
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-06-21 12:41:08 -0400

    add login form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,25 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import PageLayout from '../components/PageLayout';
+
+import {
+  Container,
+  Typography,
+  TextField,
+  Link,
+  Button,
+  Grid,
+} from "@material-ui/core";
 
 
-export default function Login({ }) {
+/**
+ * @function onSignIn googleUser.getBasicProfile()
+ * @params googleUser {}
+ * @interface googleUser.getBasicProfile() {     
+        getId()); 
+        getName());
+        getGivenName());
+        getFamilyName());
+        getImageUrl());
+        getEmail()); 
+      }
+ */
 
-  function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
 
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-  }
+export default function Login() {
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  // [event.target.id]: event.target.value,
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    // axios
+    //   .post("/api/login", user )
+    //   .then(() => {})
+    //   .catch(error => console.log(error));
+  };
+
   return (
-    <div className="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-
+    <PageLayout>
+      <Container>
+        <Typography component="h1" variant="h4">
+          Log In With Your Google Account
+        </Typography>
+        <div className="g-signin2" data-onsuccess="onSignIn" data-theme="dark" />
+        <Typography component="h1" variant="h4">
+          Log In With Your Accounts Credentials
+        </Typography>
+        <form
+          onSubmit={e => handleSubmit(e)}
+        ></form>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              variant="outlined"
+              id="email"
+              type="email"
+              label="Email Address"
+              value={user.email}
+              onChange={setUser}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              variant="outlined"
+              label="Password"
+              type="password"
+              id="password"
+              value={user.password}
+              onChange={setUser}
+            />
+          </Grid>
+        </Grid>
+        <Button
+          fullWidth
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Log In
+            </Button>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Link href="/register" variant="body2">
+              Don't have an account? Sign up
+          </Link>
+          </Grid>
+        </Grid>
+      </Container>
+    </PageLayout>
   )
 
 }
