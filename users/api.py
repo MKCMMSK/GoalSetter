b651432883e486948871441488443c3dd38b325a
Shao Kang Mai <Colinmaisk@gmail.com>
2020-06-23 19:01:20 -0700

    started user backend

--- users/api.py
+++ users/api.py
@@ -1,9 +1,13 @@
 from users.models import User
 from rest_framework import viewsets, permissions
 from .serializers import UserSerializer
+from rest_framework import request
+from rest_framework.decorators import action
+from rest_framework.response import Response
+import json
 
 class UserViewSet(viewsets.ModelViewSet):
-    # queryset = User.objects.all()
+    queryset = User.objects.all()
     permission_classes = [
         permissions.AllowAny
     ]
@@ -11,7 +15,12 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         deconstruct = json.loads(self.request.body.decode('ascii'))
-        currentProject = Project.objects.get(id = deconstruct["projectId"])
-        listOfTasks = currentProject.task_set.all()
+        # currentProject = Project.objects.get(id = deconstruct["projectId"])
+        # listOfTasks = currentProject.task_set.all()
         
-        return listOfTasks
+        return 
+    @action(detail=True)
+    def checkForUser(self, request, pk=None):
+        currentUserEmail = self.request.body.decode('ascii')
+        print(currentUserEmail, "this is test")
+        return Response(currentUserEmail)
