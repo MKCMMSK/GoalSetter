8f91e0c89f69d7e822de9a21dbd5f191a5bfc157
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-06-30 15:22:41 -0400

    add react google login

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 //components
 import PageLayout from '../components/PageLayout';
 import Tasks from "../components/Tasks"
+import GoogleLoginButton from "../components/GoogleLogin"
 
 //hooks
 import useApplicationData from "../components/hooks/useApplicationData"
@@ -46,6 +47,7 @@ const HomePage = () => {
 
       <section className={classes.root}>
         <Tasks  projectsList={projectsList} tasksList={tasksList}/>
+        <GoogleLoginButton/>
       </section>
     </PageLayout>
   )
